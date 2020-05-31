4beb4c3df7de7d8ff5d56c3e00f4fda42ebca76f
AndreyMartinez <ram.v98@hotmail.com>
2020-05-31 06:30:19 -0500

    Raphael Martinez: feat google maps

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,15 +4,16 @@
   import RegisterPage from './pages/registerPage/RegisterPage.svelte'
   import LoginPage from './pages/loginPage/LoginPage.svelte'
   import DisponserPage from './pages/disponserPage/DisponserPage.svelte'
+  import IniciativePage from './pages/iniciativePage/IniciativePage.svelte'
   export let url = "";
 </script>
 
 <Router url="{url}">
 <Route path="register" component="{RegisterPage}" />
 <Route path="login" component="{LoginPage}" />
-
+<Route path="iniciative" component="{IniciativePage}"/>
 <Route path="recycler" component="{DisponserPage }"/>
-    <Route path="/"><MainPage /></Route>
+    <Route path="/"><IniciativePage /></Route>
 </Router>
 
 
